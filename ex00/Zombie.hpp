e2e7862a1b02eb284b641b83218811154bb8f8ce
Matias Adrian Grillo <mgrillo@c1r2s2.42madrid.com>
2026-02-23 13:33:44 +0100

    ex01 prog

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+
+	public:
+		Zombie();
+		Zombie(std::string name)
+		~Zombie()
+		void	announce(void)
+
+};
+
+// Zombie	*newZombie(std::string name);
+// void	randomChump(std::string name);
---
+++ ex00/newZombie.cpp
@@ -0,0 +1,8 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(){}
+
+Zombie::Zombie(std::string s)
+{
+
+}
